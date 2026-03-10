7a7f3a9fa2aeefb0158b0450a5d341a18605eafb
unknown <mariajuliadejesusbarbosa6@gmail.com>
2026-03-10 09:22:14 -0300

    Script do git

---
+++ aula4.py
@@ -0,0 +1,10 @@
+vetores = [0] * 5
+for i in range (0,len(vetores)):
+ vetores[i] = input("Nome do aluno:")
+
+for i in range(0, len(vetores)):
+    print("Aluno", i + 1, "-", vetores[i])
+
+numeros = [0] * 11
+for m in range(0, len(numeros)):
+    print("Números", numeros[m])
---
+++ github.py
@@ -0,0 +1,21 @@
+import os 
+print ("Configurando email")
+comando_email = "git config user.email \"mariajuliadejesusbarbosa6@gmail.com\""
+os.system(comando_email)
+
+print ("Adicionando modificações....")
+comando1 = "git add *"
+os.system(comando1)
+
+mensagem = input("Mensagem no commit: ")
+while (len(mensagem) < 5 ):
+    print ("⚠️Mensagem muito pequena, detalhe mais...")
+    mensagem = input("Mensagem no commit: ")
+
+print ("✅Registrando alterações..")
+comando2 = "git commit -m '" + mensagem + "'"
+os.system(comando2)
+
+print ("🛜Enviando projeto pro github..")
+comando3 = "git push" 
+os.system(comando3)
